ddb36271a8ea85dde320d8ac2c84455078225042
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-09-28 01:55:23 -0300

    Started project

---
+++ src/main/java/com/example/netshoeswebscrapping/NetshoesWebScrappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.netshoeswebscrapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetshoesWebScrappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetshoesWebScrappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netshoeswebscrapping/NetshoesWebScrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netshoeswebscrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetshoesWebScrappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
